fcf5277d8821a2076110c6753e322864022f9346
arthurlbo <arthurlbo16@gmail.com>
2023-05-14 21:12:09 -0300

    feat: form components

---
+++ src/components/LoginForm/Form/Form.tsx
@@ -0,0 +1,12 @@
+
+import { ErrorMessage } from "./ErrorMessage";
+import { Field } from "./Field";
+import { Input } from "./Input";
+import { Label } from "./Label";
+
+export const Form = {
+    Label,
+    Input,
+    ErrorMessage,
+    Field,
+};
